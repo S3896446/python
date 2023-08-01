f5725c5d0ce38b1b3b1ae6510d3b0ff750120b45
S3896446 <s3896446@student.rmit.edu.au>
2023-08-01 15:19:19 +1000

    Lab3Q1.1

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("hello")
+import json
+
+data = {
+    "user" : {
+        "name" : "J",
+        "phonenumber" : "0231231333",
+        "age": "21",
+        "address" : "rmit" 
+    }
+}
+
+with open("user.json", "w") as write:
+    json.dump(data, write)
---
+++ person.py
@@ -0,0 +1,9 @@
+import json
+
+#with open("./PIoT_TL3_code/person.json", "r") as read_content:
+#	print(json.load(read_content))
+
+o = open("./PIoT_TL3_code/person.json")
+read_content = json.load(o)
+l = read_content["firstName"] 
+print(l)
