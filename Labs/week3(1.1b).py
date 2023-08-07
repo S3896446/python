3bf0f8f9692e270098549b9c28727c5108336d7c
S3896446 <s3896446@student.rmit.edu.au>
2023-08-07 17:44:01 +1000

    movement

--- Labs/week3(1.1b).py
+++ Labs/week3(1.1b).py
@@ -3,7 +3,7 @@ import json
 #with open("./PIoT_TL3_code/person.json", "r") as read_content:
 #	print(json.load(read_content))
 
-o = open("./PIoT_TL3_code/person.json")
+o = open("PIoT_TL3_code/person.json")
 read_content = json.load(o)
 l = read_content["firstName"] 
 print(l)
